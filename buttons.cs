79c9f7f4bb953c33b496af8c9394ca9ff4f0f6da
Riviere <rjs76@njit.edugit config --global user.name Rivieregit config --global user.email rjs76@njit.edu>
2019-06-08 16:01:26 -0400

    Menus Work

---
+++ buttons.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Playermenu()
+    {
+        SceneManager.LoadScene("player menu");
+
+    }
+    public void Playercontrols()
+    {
+        SceneManager.LoadScene("Playercontrols");
+    }
+    public void PlayerMark()
+    {
+        SceneManager.LoadScene("MarkMenu");
+    }
+    public void DifficultyMenu()
+    {
+        SceneManager.LoadScene("DifficultyMenu");
+    }
+    public void QuitNOW()
+    {
+        Application.Quit();
+    } 
+}
